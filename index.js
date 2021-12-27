3c90d71366db54937fc8c603df728ad672864cb8
mohamadalmell <mohamadalmell@gmail.com>
2021-12-27 18:18:07 +0200

    Step 3

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,7 +7,22 @@ app.get('/', (req,res)=>{
     res.send('ok')
 })
 
+app.get('/test', (req,res)=>{
+    res.send({
+        status: 200,
+        message: 'ok'
+    })
+})
+
+var time = new Date();
+
+app.get('/time', (req,res)=>{
+    res.send({
+        status:200,
+        message: `${time.getHours()}:${time.getMinutes()}`
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
   }) 
->>>>>>> dd074d0 (Step 2)
